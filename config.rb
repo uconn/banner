2050292e4220bc8349d2b6dc8a40c786f8a32593
Brian Daley <brian.daley@uconn.edu>
2013-07-25 14:31:07 -0400

    Created partials and included webfonts in css.

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'compass-flexbox'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,7 +9,7 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed #:expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
