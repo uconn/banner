4fd2661ef19da3b27c57853177bf8006b696901b
Adam Berkowitz <adam.berkowitz@uconn.edu>
2020-01-21 11:00:49 -0500

    working on style and conditional loading

--- _site/js/site.js
+++ _site/js/site.js
@@ -1,63 +1,63 @@
-const forms = Array.from(document.querySelectorAll('main form'))
-const toggles = Array.from(document.querySelectorAll('select'))
-const widthForm = document.querySelector('#container-width-form')
+(() => {
+  const forms = Array.from(document.querySelectorAll('main form'))
+  const selects = Array.from(document.querySelectorAll('select'))
+  const radioButtons = Array.from(document.querySelectorAll('input[type=radio]'))
+  const toggles = [...selects, ...radioButtons]
+  const widthForm = document.querySelector('#container-width-form')
 
-forms.map(form => {
-  form.addEventListener('submit', (evt) => {
-    evt.preventDefault()
-    switch (evt.target.id) {
-      case 'alternative-banner-form':
-        setType()
-        break;
-      case 'container-width-form':
-        const width = evt.target.querySelector('#container-width').value
-        setWidth(width)
-        displayWidth(width)
-        break;
-    
-      default:
-        break;
-    }
+  forms.map(form => {
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault()
+      switch (evt.target.id) {
+        case 'container-width-form':
+          const width = evt.target.querySelector('#container-width').value
+          setWidth(width)
+          displayWidth(width)
+          break;
+
+        default:
+          break;
+      }
+    })
   })
-})
 
-toggles.map(toggle => {
-  toggle.addEventListener('change', (evt) => {
-    switch (evt.target.id) {
-      case 'alternative-checker':
-        
-        break;
-    
-      case 'theme-switcher':
-        setTheme(evt.target.value)
-        break;
+  toggles.map(toggle => {
+    toggle.addEventListener('change', (evt) => {
+      switch (evt.target.id) {
+        case 'alternative-checker':
 
-      default:
-        break;
-    }
+          break;
+
+        case 'standard-theme':
+        case 'inverted-theme':
+          setTheme(evt.target.value)
+          break;
+
+        default:
+          break;
+      }
+    })
   })
-})
 
-const setType = () => {
-  console.log(evt.target)
-}
-
-const setTheme = (theme) => {
-  const headerContainer = document.querySelector('#uconn-header-container')
-  if (theme === 'inverted') {
-    headerContainer.classList.add('white')
-  } else {
-    headerContainer.classList.remove('white')
+  const setTheme = (theme) => {
+    const headerContainer = document.querySelector('#uconn-header-container')
+    if (theme.includes('inverted')) {
+      headerContainer.classList.add('white')
+    } else {
+      headerContainer.classList.remove('white')
+    }
   }
-}
 
-const setWidth = width => {
-  document.documentElement.style.setProperty('--container-width', `${width}px`)
-}
+  const setWidth = width => {
+    document.documentElement.style.setProperty('--container-width', `${width}px`)
+  }
 
-const displayWidth = width => {
-  const dataWidths = Array.from(document.querySelectorAll('.data-width'))
-  dataWidths.map(span => {
-    span.innerHTML = `${width}px`
-  })
-}
+  const displayWidth = width => {
+    const dataWidths = Array.from(document.querySelectorAll('.data-width'))
+    dataWidths.map(span => {
+      span.innerHTML = `${width}px`
+    })
+  }
+
+})()
+
