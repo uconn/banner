8fb360e0891185859e3812ac63d2be0f87b1c92e
Adam Berkowitz <adam.berkowitz@uconn.edu>
2022-11-17 13:01:51 -0500

    new cli that does not throw deprecation warnings

--- src/Banner/Generator.php
+++ src/Banner/Generator.php
@@ -8,57 +8,114 @@ require 'Banner.php';
 use League\CLImate\CLImate as Cli;
 
 class Generator {
-  protected $args;
 
-  public function createArguments() {
-    global $argv;
-    $cli = new Cli();
-    $cli->description('Small command line utility to generate UConn banner markup');
-    $cli->arguments->add([
+  protected $cli;
+
+  public function __construct()
+  {
+    $this->cli = new Cli();
+    $this->cli->description('Small command line utility to generate UConn banner markup');
+    $this->cli->arguments->add([
+      'help' => [
+        'longPrefix'  => 'help',
+        'description' => 'Prints a usage statement',
+        'noValue'     => true,
+      ],
       'path' => [
         'prefix' => 'p',
         'longPrefix' => 'path',
-        'description' => 'File path with name to save banner',
+        'description' => 'File path with name to save banner (ex ./my-banner.html)',
         'required' => true,
         'defaultValue' => './'
+      ],
+      'school-college' => [
+        'prefix' => 's',
+        'longPrefix' => 'school-college',
+        'description' => 'The school/college to display'
+      ],
+      'abbreviation' => [
+        'prefix' => 'a',
+        'longPrefix' => 'abbreviation',
+        'description' => 'An abbreviation for long school/college names. Will be visible on mobile devices'
+      ],
+      'url' => [
+        'prefix' => 'u',
+        'longPrefix' => 'url',
+        'description' => 'School/college url'
+      ],
+      'alternative' => [
+        'prefix' => 'v',
+        'longPrefix' => 'alternative',
+        'description' => 'Whether to use the school and college banner type',
+        'defaultValue' => false,
+        'castTo' => 'bool'
+      ],
+      'a-z-dropdown' => [
+        'longPrefix' => 'a-z-dropdown',
+        'description' => 'Whether to add markup for an A-Z dropdown/popup. Default - false',
+        'defaultValue' => false,
+        'castTo' => 'bool'
+      ],
+      'a-z-url' => [
+        'longPrefix' => 'a-z-url',
+        'description' => 'Url for the site\'s a-z index page'
+      ],
+      'use-mobile' => [
+        'shortPrefix' => 'm',
+        'longPrefix' => 'use-mobile',
+        'description' => 'Whether to use a mobile menu icon in the banner. Default - false',
+        'defaultValue' => false,
+        'castTo' => 'bool'
+      ],
+      'mobile-id' => [
+        'longPrefix' => 'mobile-id',
+        'description' => 'Set an ID attribute for the mobile menu',
+      ],
+      'header' => [
+        'prefix' => 'r',
+        'longPrefix' => 'header',
+        'description' => 'Alternative site header'
+      ],
+      'invert-banner-color' => [
+        'prefix' => 'i',
+        'longPrefix' => 'invert-banner-color',
+        'description' => 'Whether to use a white banner with blue text',
+        'castTo' => 'bool'
       ]
     ]);
+    $this->cli->arguments->parse();
+  }
 
-
-
-
-    $cli->description('Small command line utility to generate UConn banner markup')
-      ->opt('path:p', 'File path with name to save banner', true)
-      ->opt('school-college:s', 'The school/college to display', false, 'string')
-      ->opt('abbreviation:a', 'An abbreviation for long school/college names. Will be visible on mobile devices', false)
-      ->opt('url:u', 'School/college url', false)
-      ->opt('alternative:v', 'Whether to use the school and college banner type', false, 'bool')
-      ->opt('a-z-dropdown', 'Whether to add markup for an A-Z dropdown/popup. Default - false', false, 'bool')
-      ->opt('a-z-url', 'Url for the site\'s a-z index page', false)
-      ->opt('use-mobile:m', 'Whether to use a mobile menu icon in the banner. Default - false', false, 'bool')
-      ->opt('mobile-id', 'Set an ID attribute for the mobile menu', false, 'string')
-      ->opt('header:r', 'Alternative site header', false)
-      ->opt('invert-banner-color:i', 'Whether to use a white banner with blue text', false, 'bool');
-
-    $this->args = $cli->parse($argv, true);
+  public function getCli() {
+    return $this->cli;
   }
 
   public function outputBannerToFile() {
     $banner = new Banner();
-    $banner->school = $this->args->getOpt('school-college');
-    $banner->school_abbreviation = $this->args->getOpt('abbreviation');
-    $banner->url = $this->args->getOpt('url');
-    $banner->alternative = $this->args->getOpt('alternative', false);
-    $banner->a_z_dropdown = $this->args->getOpt('a-z-dropdown', false);
-    $banner->a_z_url = $this->args->getOpt('a-z-url');
-    $banner->use_mobile_menu =  $this->args->getOpt('use-mobile', false);
-    $banner->mobile_menu_id = $this->args->getOpt('mobile-id');
-    $banner->header = $this->args->getOpt('header');
-    $banner->invert_banner_color = $this->args->getOpt('invert-banner-color');
-    file_put_contents($this->args->getOpt('path'), $banner);
+    $banner->school = $this->cli->arguments->get('school-college');
+    $banner->school_abbreviation = $this->cli->arguments->get('abbreviation');
+    $banner->url = $this->cli->arguments->get('url');
+    $banner->alternative = $this->cli->arguments->get('alternative');
+    $banner->a_z_dropdown = $this->cli->arguments->get('a-z-dropdown');
+    $banner->a_z_url = $this->cli->arguments->get('a-z-url');
+    $banner->use_mobile_menu =  $this->cli->arguments->get('use-mobile');
+    $banner->mobile_menu_id = $this->cli->arguments->get('mobile-id');
+    $banner->header = $this->cli->arguments->get('header');
+    $banner->invert_banner_color = $this->cli->arguments->get('invert-banner-color');
+
+    // strip the head matter from the final output
+    $pattern = '/(---[\n\r])[\w:. ]*/i';
+    $banner = preg_replace($pattern, '', $banner);
+
+    file_put_contents($this->cli->arguments->get('path'), $banner);
   }
 }
 
 $g = new Generator();
-$g->createArguments();
-$g->outputBannerToFile();
+$cli = $g->getCli();
+
+if ($cli->arguments->get('help')) {
+  $cli->usage();
+} else {
+  $g->outputBannerToFile();
+}
